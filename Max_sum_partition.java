0c0a8b9b91d5a2da28ff5f35df7b8ea50bae2bf1
Advait Churi <advaitchuri@Advaits-MacBook-Pro.local>
2024-07-18 20:15:15 -0700

    Done DP-4

---
+++ Max_sum_partition.java
@@ -0,0 +1,28 @@
+// Time Complexity : O(n*k)
+// Space Complexity : O(n) 
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+class Solution {
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        if(arr == null || arr.length ==0){
+            return 0;
+        }
+        int n = arr.length;
+        int dp[] = new int[n];
+        dp[0] = arr[0];
+        for(int i =1; i<n; i++){
+            int max = arr[i];
+            for(int j = 1; j <= k && i - j +1 >= 0; j++){
+                max = Math.max(max, arr[i-j +1]);
+                if(i-j>= 0){
+                dp[i] = Math.max(dp[i], dp[i-j] + j * max);
+                }
+                else{
+                    dp[i] = Math.max(max, j*max);
+                }
+            }
+        }
+        return dp[n-1];
+    }
+}
