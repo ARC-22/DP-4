0a81497257701a15e2edc2d445c757e33e479ea6
Advait Churi <advaitchuri@Advaits-MacBook-Pro.local>
2024-07-19 20:11:36 -0700

    Corrected TC/SC

--- Max_sum_partition.java
+++ Max_sum_partition.java
@@ -1,5 +1,5 @@
-// Time Complexity : O(n^3)
-// Space Complexity : O(n+k) // string length + k words hashset 
+// Time Complexity : O(n*k)
+// Space Complexity : O(n) 
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 
